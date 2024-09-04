3589f4d6ed1a530a6d690b309a8819f0c615f17d
faustinok <faustinoknada@gmail.com>
2024-09-04 15:38:28 -0300

    projeto de image processing

---
+++ bootcamp/image-processing-package/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name = "",
+    version = "",
+    author= "",
+    author_email="",
+    description="",
+    long_description= "",
+    long_description_content_type= " ",
+    url = "",
+    packages= find_packages(),
+    install_requires= requirements,
+    python_requires = '>=3.12.4'
+
+)
